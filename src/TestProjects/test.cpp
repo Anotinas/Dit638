f78935bdb9c173883e16c9bdb763682755aa2c6f
unknown <daviddaviddele@gmail.com>
2019-08-26 23:42:31 +0200

    added assets & tests for car detection

--- src/TestProjects/test.cpp
+++ src/TestProjects/test.cpp
@@ -15,18 +15,25 @@ TEST(CameraAquisitonTest, GetVideoFrame) {
 
 
 TEST(RecognitionTest, Detect9Car) {
-	cv::Mat frame = cv::imread(sourceDirectory+"/TestProjects/Assets/mask.jpg");
-	
-	
+	cv::Mat frame = cv::imread(sourceDirectory+"/TestProjects/Assets/CarLeft.png");
+	std::vector<tracking::Object> cars = tracking::detectObjects(HSV,frame);
+	tracking::Object o = detectCarAt9oclock(cars);
+	ASSERT_TRUE(o.position.x != -1 && o.position.y !=-1 && o.area != -1);
+
 }
 
 TEST(RecognitionTest, Detect3Car) {
-	cv::Mat frame = cv::imread(sourceDirectory+"/TestProjects/Assets/mask.jpg");	
+	cv::Mat frame = cv::imread(sourceDirectory+"/TestProjects/Assets/CarCenter.png");
+	std::vector<tracking::Object> cars = tracking::detectObjects(HSV,frame);
+	tracking::Object o = detectCarAt3oclock(cars);;
+	ASSERT_TRUE(o.position.x != -1 && o.position.y !=-1 && o.area != -1);
 }
 
 TEST(RecognitionTest, Detect12Car) {
-	cv::Mat frame = cv::imread(sourceDirectory+"/TestProjects/Assets/mask.jpg");
-
+	cv::Mat frame = cv::imread(sourceDirectory+"/TestProjects/Assets/CarRight.png");
+	std::vector<tracking::Object> cars = tracking::detectObjects(HSV,frame);
+	tracking::Object o = detectCarAt12oclock(cars);;
+	ASSERT_TRUE(o.position.x != -1 && o.position.y !=-1 && o.area != -1);
 }
 TEST(ClassificationTest, DetectStopsignTrue){
 	cv::Mat frame = cv::imread(sourceDirectory+"/TestProjects/Assets/stopSign.png");
